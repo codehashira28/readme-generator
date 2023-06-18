92d0c9ee150648541923ad020fb09c2614b98d61
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-06-18 14:45:57 -0500

    imported the JavaScript file to generate README and added complete functionality to generate README based on user input.

--- index.js
+++ index.js
@@ -2,12 +2,71 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        message: 'Briefly describe your project',
+        name: 'description',
+    },
+    {
+        message: 'Are there any installation instructions for your project?',
+        name: 'installation',
+    },
+    {
+        message: 'What are the usage instructions?',
+        name: 'usage',
+    },
+    {
+        message: 'What are the project contribution guidelines',
+        name: 'contributions',
+    },
+    {
+        message: 'Tests?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Please choose a license',
+        name: 'license',
+        choices: ['Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT License',
+            'BSD 2-Clause "Simplified" License',
+            'BSD 3-Clause "New" or "Revised" License',
+            'Boost Software License 1.0',
+            'GNU General Public License v2.0', 
+            'Mozilla Public License 2.0',
+            'The Unlicense',
+            'No license'
+        ]
+    },
+    {
+        message: 'Please enter github username',
+        name: 'username',
+    },
+    {
+        message: 'Please enter email address',
+        name: 'email'
+    }
+
+];
+
+
+prompt(questions).then((data) => {
+        const readme = markdown(data);
+        writeToFile('GeneratedREADME.md', readme);
+    })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,112 @@
+const licenses = {
+  'Apache License 2.0': {
+      link: 'https://opensource.org/licenses/Apache-2.0',
+      badge: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
+  },
+  'GNU General Public License v3.0': {
+      link: 'https://www.gnu.org/licenses/gpl-3.0',
+      badge:'https://img.shields.io/badge/License-GPLv3-blue.svg'
+  },
+  'MIT License': {
+      link: 'https://opensource.org/license/mit',
+      badge: 'https://img.shields.io/badge/License-MIT-yellow.svg'
+  },
+  'BSD 2-Clause "Simplified" License': {
+      link: 'https://opensource.org/licenses/BSD-2-Clause',
+      badge: 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg'
+  },
+  'BSD 3-Clause "New" or "Revised" License': {
+      link: 'https://opensource.org/licenses/BSD-3-Clause',
+      badge: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg'
+  },
+  'Boost Software License 1.0': {
+      link: 'https://www.boost.org/LICENSE_1_0.txt',
+      badge: 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg'
+  },
+  'GNU General Public License v2.0': {
+      link: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html',
+      badge: 'https://img.shields.io/badge/License-GPL_v2-blue.svg'
+  },
+  'Mozilla Public License 2.0': {
+      link: 'https://opensource.org/licenses/MPL-2.0',
+      badge: 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg'
+  },
+  'The Unlicense': {
+      link: 'http://unlicense.org',
+      badge: 'https://img.shields.io/badge/license-Unlicense-blue.svg'
+  }
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'No license') {
+    return "";
+  }
+  return `[![](${licenses[license].badge})](${licenses[license].link})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `This project is covered under [${license}](${licenses[license].link}).`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === 'No license') {
+    return "";
+  }
+
+  return `## License
+  
+  ${renderLicenseLink(license)}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.description}
+  
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+  ${data.contributions}
+
+  ## Tests
+
+  ## Questions
+
+  If there any questions concerning the project, feel free to contact me by either of the following ways:
+
+  Github Profile: [${data.username}](https://github.com/${data.username})
+  
+  Email: ${data.email}.
 `;
 }
 
